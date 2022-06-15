500098d01238cf26b46037bc735e01c088491d23
Angelina Zhang <angelinaz@fb.com>
2022-06-15 15:12:39 -0700

    Completed app launch screen. Retrieved data from Movie API and TableView displays movie names

---
+++ Flixter/MovieViewController.m
@@ -0,0 +1,65 @@
+//
+//  MovieViewController.m
+//  Flixter
+//
+//  Created by Angelina Zhang on 6/15/22.
+//
+
+#import "MovieViewController.h"
+#import "TableViewCell.h"
+// #import "UIImageView+AFNetworking.h"
+
+@interface MovieViewController () <UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+
+@property (nonatomic, strong) NSArray *movieData;
+@end
+
+@implementation MovieViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.tableView.dataSource = self;
+    // self.tableView.delegate = self;
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=290a6c40d7e173d0df08968468e7af89"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               self.movieData = dataDictionary[@"results"];
+               // NSLog(@"%@", self.movieData);
+               [self.tableView reloadData];
+           }
+       }];
+    [task resume];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    TableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"customMovieCell" forIndexPath:indexPath];
+    
+    // Update title and synopsis
+    cell.titleLabel.text = self.movieData[indexPath.row][@"title"];
+    cell.synopsisLabel.text = self.movieData[indexPath.row][@"overview"];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.movieData.count;
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+@end
